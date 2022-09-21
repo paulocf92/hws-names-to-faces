fc205905934cfac67d3b699d7f3388a737777888
Paulo Filho <pcf.feat@gmail.com>
2022-09-21 15:37:47 -0400

    challenge done

--- NamesToFaces/ViewController.swift
+++ NamesToFaces/ViewController.swift
@@ -42,6 +42,9 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     
     @objc func addNewPerson() {
         let picker = UIImagePickerController()
+//        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+//            picker.sourceType = .camera
+//        }
         picker.allowsEditing = true
         picker.delegate = self
         present(picker, animated: true)
@@ -72,6 +75,22 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let person = people[indexPath.item]
         
+        let ac = UIAlertController(title: "Edit person", message: "What would you like to do?", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Rename", style: .default) { [weak self] _ in
+            self?.renamePerson(person: person)
+        })
+        ac.addAction(UIAlertAction(title: "Delete", style: .destructive) { [weak self] _ in
+            self?.people.remove(at: indexPath.item)
+            self?.collectionView.reloadData()
+        })
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(ac, animated: true)
+    }
+    
+    func renamePerson(person: Person) {
         let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
         ac.addTextField()
         
